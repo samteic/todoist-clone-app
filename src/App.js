dec4983eed138ea9e996cd06224b685641c66e88
Ofordile Ebuka Samuel <ebukasam108@gmail.com>
2024-01-24 11:52:35 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import react from "react"
+
+function App() {
+  return (
+    <div className="App">
+     <h2 className="text-green-500 font-bold">hello world</h2>
+    </div>
+  );
+}
+
+export default App;
